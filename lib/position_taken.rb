29846e3c511afd38e0cef3e58cc0997dfe9c3fa0
Kriti Rai <kriti.rai@gmail.com>
2017-11-17 05:38:02 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 def position_taken?(board, index)
-  if board[index] == " " or board[index] == "" board[index] == nil
+  if board[index] == " " || "" || nil
     return false
   else
     return true
