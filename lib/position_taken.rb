ffa848d65c9d31afeef9f545212caaf533b63b87
Kriti Rai <kriti.rai@gmail.com>
2017-11-17 05:43:26 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
   if board[index] == " " || "" || nil
     return false
-  else
+  elseif board[index] == "X" || "O"
     return true
   end
 end# code your #position_taken? method here!
